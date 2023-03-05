6fdf25624c4e8c314f2e2745acdfaec842ad30bf
lucaislourenco <lucaspeixe2018@gmail.com>
2023-03-05 12:39:21 -0300

    Realizada a criação da página de Login User

--- vetsystemtech-frontend/src/app/app-routing.module.ts
+++ vetsystemtech-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { UserLoginComponent } from './components/user/user-login/user-login.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'user/login', component: UserLoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- vetsystemtech-frontend/src/app/app.module.ts
+++ vetsystemtech-frontend/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from 'src/shared/app-material/app-material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppMaterialModule } from 'src/shared/app-material/app-material.module';
+import { UserLoginComponent } from './components/user/user-login/user-login.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
