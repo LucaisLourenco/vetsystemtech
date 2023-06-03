91a0a0c3c145ee166d2a6e7b376e6ec59440c9a2
lucaislourenco <lucaspeixe2018@gmail.com>
2023-06-03 12:38:40 -0300

    Caixas de diálogo.

--- vetsystemtech-frontend/src/app/shared/shared.module.ts
+++ vetsystemtech-frontend/src/app/shared/shared.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppMaterialModule } from './app-material/app-material.module';
+import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
+import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ErrorDialogComponent,
+    ConfirmationDialogComponent
+  ],
   imports: [
     CommonModule,
     AppMaterialModule
