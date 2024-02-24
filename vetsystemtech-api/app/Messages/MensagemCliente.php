f7d2fca2c2cbd78c6e28bdcb49d443826acae839
lucas.lourenco <lucaspeixe2018@gmail.com>
2024-02-24 19:36:13 -0300

    Mehoria

---
+++ vetsystemtech-api/app/Messages/MensagemCliente.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Messages;
+
+class MensagemCliente
+{
+    const CLT001 = 'Credenciais inválidas';
+    const CLT002 = 'Não foi possível criar o Token.';
+    const CLT003 = 'Não foi possível obter as informações do usuário.';
+    const CLT004 = 'Não foi possível realizar logoff.';
+    const CLT005 = 'Cliente desconectado com sucesso.';
+}
---
+++ vetsystemtech-api/app/Messages/MensagemVeterinario.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Messages;
+
+class MensagemVeterinario
+{
+    const VTR001 = 'Credenciais inválidas';
+    const VTR002 = 'Não foi possível criar o Token.';
+    const VTR003 = 'Não foi possível obter as informações do usuário.';
+    const VTR004 = 'Não foi possível realizar logoff.';
+    const VTR005 = 'Veterinário desconectado com sucesso.';
+}
