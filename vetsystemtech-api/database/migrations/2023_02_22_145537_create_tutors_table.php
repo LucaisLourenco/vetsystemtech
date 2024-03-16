7c6404108c00af998170b416509e26f6d564b199
lucas.lourenco <lucaspeixe2018@gmail.com>
2024-03-16 11:34:43 -0300

    Ajuste de tabelas

--- vetsystemtech-api/database/migrations/2023_02_22_145537_create_tutors_table.php
+++ vetsystemtech-api/database/migrations/2023_02_22_145537_create_tutors_table.php
@@ -6,23 +6,43 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    const TABLE = 'tutors';
+
+    public static string $id = 'id';
+    public static string $name = 'name';
+    public static string $username = 'username';
+    public static string $cpf = 'cpf';
+    public static string $email = 'email';
+    public static string $birth = 'birth';
+    public static string $emailVerifiedAt = 'email_verified_at';
+    public static string $active = 'active';
+    public static string $password = 'password';
+    public static string $idRole = 'role_id';
+    public static string $idGender = 'gender_id';
+    public static string $asTableRoles = 'roles';
+    public static string $asTableGenders = 'genders';
+    public static string $onColumnRoles = 'id';
+    public static string $onColumnGenders = 'id';
+
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('tutors', function (Blueprint $table) {
+        Schema::create(self::TABLE, function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username')->unique();
-            $table->string('cpf')->unique();
-            $table->unsignedBigInteger('gender_id');
-            $table->foreign('gender_id')->references('id')->on('genders');
-            $table->string('email')->unique();
-            $table->date('birth');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('active');
+            $table->string(static::$name);
+            $table->string(static::$username)->unique();
+            $table->string(static::$cpf)->unique();
+            $table->unsignedBigInteger(static::$idRole);
+            $table->foreign(static::$idRole)->references(static::$onColumnRoles)->on(static::$asTableRoles);
+            $table->unsignedBigInteger(static::$idGender);
+            $table->foreign(static::$idGender)->references(static::$onColumnGenders)->on(static::$asTableGenders);
+            $table->string(static::$email)->unique();
+            $table->date(static::$birth)->nullable();
+            $table->timestamp(static::$emailVerifiedAt)->nullable();
+            $table->string(static::$password);
+            $table->integer(static::$active);
             $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
