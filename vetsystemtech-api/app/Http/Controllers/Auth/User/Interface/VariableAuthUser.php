af90878da012b454c47d0ec3f577c55a0d1c94ef
lucas.lourenco <lucaspeixe2018@gmail.com>
2024-03-16 18:09:15 -0300

    Ajuste de tabelas

--- vetsystemtech-api/app/Http/Controllers/Auth/User/Interface/VariableAuthUser.php
+++ vetsystemtech-api/app/Http/Controllers/Auth/User/Interface/VariableAuthUser.php
@@ -6,5 +6,10 @@ use App\Http\Controllers\Obrigatorio\Interface\VariableRequired;
 
 interface VariableAuthUser extends VariableRequired
 {
-
+    const PASSWORD = 'password';
+    const USERNAME = 'username';
+    const ERROR = 'error';
+    const TOKEN = 'token';
+    const MESSAGE = 'message';
+    const API = 'api';
 }
