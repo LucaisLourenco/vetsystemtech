62bec818e4b5d190e001696d8c237edcfe9209e9
lucas.lourenco <lucaspeixe2018@gmail.com>
2024-03-17 12:48:08 -0300

    Melhoria no título

---
+++ vetsystemtech-frontend/src/app/management/tutors/model/tutor.ts
@@ -0,0 +1,11 @@
+export interface Tutor {
+  id: number;
+  name: string;
+  username: string;
+  cpf: number;
+  gender_id: string;
+  email: string;
+  birth: string;
+  password: string;
+  active: number;
+}
