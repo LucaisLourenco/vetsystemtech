e4e503f142f047a04ea4b36cbf8c2d6f9f82cee1
lucas.lourenco <lucaspeixe2018@gmail.com>
2024-03-17 20:46:22 -0300

    Lista de tutores

--- vetsystemtech-frontend/src/app/management/tutors/tutors.component.spec.ts
+++ vetsystemtech-frontend/src/app/management/tutors/tutors.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TutorsComponent } from './tutors.component';
+import {TutorsComponent} from './tutors.component';
 
 describe('TutorsComponent', () => {
   let component: TutorsComponent;
