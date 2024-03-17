7e01280d40cf82dbe43d3425517f5e7e2a8b0732
lucas.lourenco <lucaspeixe2018@gmail.com>
2024-03-17 14:49:15 -0300

    Lista de tutores

--- vetsystemtech-frontend/src/app/management/tutors/services/tutors.service.ts
+++ vetsystemtech-frontend/src/app/management/tutors/services/tutors.service.ts
@@ -1,9 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { first } from 'rxjs';
+import {Tutor} from "../model/tutor";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TutorsService {
 
-  constructor() { }
+  private readonly API = 'api/tutors';
+
+  constructor(private http: HttpClient) { }
+
+  list() {
+    return this.http.get<Tutor[]>(this.API).pipe(
+      first(),
+      //delay(5000),
+      //tap(responsaveis => console.log(responsaveis))
+    );
+  }
+
+  save(tutor: Partial<Tutor>) {
+    if (tutor.id) {
+      return this.update(tutor);
+    }
+
+    return this.store(tutor);
+  }
+
+  loadById(id: string) {
+    return this.http.get<Tutor>(`${this.API}/$\{id}\`)`);
+  }
+
+  private store(tutor: Partial<Tutor>) {
+    return this.http.post<Tutor>(this.API, tutor).pipe(first());
+  }
+
+  private update(tutor: Partial<Tutor>) {
+    return this.http.put<Tutor>(`${this.API}/${tutor.id}`, tutor).pipe(first());
+  }
+
+  delete(cpf: string) {
+    return this.http.delete(`${this.API}/${cpf}`).pipe(first());
+  }
 }
--- vetsystemtech-frontend/src/app/management/tutors/tutors.component.ts
+++ vetsystemtech-frontend/src/app/management/tutors/tutors.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TutorsService} from "./services/tutors.service";
+import {MatDialog} from "@angular/material/dialog";
+import {ActivatedRoute, Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {catchError} from "rxjs/operators";
+import {Observable, of} from "rxjs";
+import {ErrorDialogComponent} from "../../shared/components/error-dialog/error-dialog.component";
+import {Tutor} from "./model/tutor";
+import {ConfirmationDialogComponent} from "../../shared/components/confirmation-dialog/confirmation-dialog.component";
 
 @Component({
   selector: 'app-tutors',
@@ -6,5 +15,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./tutors.component.scss']
 })
 export class TutorsComponent {
+  constructor(
+    private tutorsService: TutorsService,
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar
+  ) {
 
+    this.refresh();
+  }
+
+  tutors$: Observable<Tutor[]> | null = null;
+
+  refresh() {
+    this.tutors$ = this.tutorsService.list().pipe(
+      catchError(error => {
+        this.onError("Ouve um erro ao carregar os dados.");
+        return of([]);
+      })
+    );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
+  onAdd() {
+    this.router.navigate(['create'], {relativeTo: this.route}).then(r => true);
+  }
+
+  onEdit(tutor: Tutor) {
+    this.router.navigate(['edit', tutor.id], {relativeTo: this.route}).then(r => true);
+  }
+
+  onDelete(tutor: Tutor) {
+
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: 'Deseja remover este responsável?',
+    });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+        this.tutorsService.delete(tutor.id).subscribe(
+          () => {
+            this.refresh();
+            this.snackBar.open('Resposável removido com sucesso.', 'x', {
+              duration: 5000,
+              verticalPosition: 'top',
+              horizontalPosition: 'center'
+            });
+          },
+          error => this.onError('Ouve um erro ao remover o responsável.')
+        )
+      }
+    });
+  }
 }
