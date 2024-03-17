d20914d496234c68b87c9577ed255282ac3a8c22
lucas.lourenco <lucaspeixe2018@gmail.com>
2024-03-17 09:41:35 -0300

    ajuste de login.

--- vetsystemtech-frontend/src/app/management/services/user/auth/auth-user.service.ts
+++ vetsystemtech-frontend/src/app/management/services/user/auth/auth-user.service.ts
@@ -4,6 +4,9 @@ import {Observable, throwError} from 'rxjs';
 import {catchError, tap} from 'rxjs/operators';
 import jwt_decode from 'jwt-decode';
 import {environment} from 'src/environments/environment.development';
+import {AppRoutes} from "../../../shared/app-routes";
+import {TOKEN_USER} from "../../../utils/constants";
+import {USER_MESSAGES} from "../../../shared/messages/user-messages";
 
 @Injectable({
   providedIn: 'root',
@@ -17,33 +20,30 @@ export class AuthUserService {
 
   login(username: string, password: string): Observable<any> {
     return this.httpClient
-      .post<any>(`${this.apiUrl}/user/login`, {username: username, password: password})
+      .post<any>(`${this.apiUrl}${AppRoutes.LOGIN}`, {username: username, password: password})
       .pipe(
         tap((response) => {
-          localStorage.setItem('userToken', response.token); // Armazena o token JWT no localStorage
+          localStorage.setItem(TOKEN_USER, response);
         }),
         catchError(this.handleError)
       );
   }
 
-  // Função para fazer o logout do usuário
   logout() {
-    localStorage.removeItem('userToken'); // Remove o token JWT do localStorage
+    localStorage.removeItem(TOKEN_USER);
     return new Observable(observer => {
-      observer.next(); // Notifica o sucesso do logoff
-      observer.complete(); // Completa o Observable
+      observer.next();
+      observer.complete();
     });
   }
 
-  // Função para verificar se o usuário está autenticado
   isAuthenticated(): boolean {
-    const token = localStorage.getItem('userToken'); // Recupera o token JWT do localStorage
+    const token = localStorage.getItem(TOKEN_USER);
     if (token) {
-      const decoded = jwt_decode(token) as any; // Decodifica o token JWT
-      const current_time = new Date().getTime() / 1000; // Converte a data atual para segundos
+      const decoded = jwt_decode(token) as any;
+      const current_time = new Date().getTime() / 1000;
       if (decoded.exp < current_time) {
-        // Verifica se o token JWT expirou
-        localStorage.removeItem('userToken'); // Remove o token JWT do localStorage
+        localStorage.removeItem(TOKEN_USER);
         return false;
       }
       return true;
@@ -51,34 +51,27 @@ export class AuthUserService {
     return false;
   }
 
-  // Função para obter o token JWT
   getToken(): string | null {
-    return localStorage.getItem('userToken') ?? null; // Retorna o token JWT do localStorage
+    return localStorage.getItem(TOKEN_USER) ?? null;
   }
 
-  // Função para lidar com os erros da requisição
   private handleError(error: any): Observable<never> {
-    let errorMessage = 'Ocorreu um erro desconhecido!';
+    let errorMessage;
 
-    // Verifica se o erro é do tipo HttpErrorResponse
     if (error.error instanceof ErrorEvent) {
-      // Erro no cliente
-      errorMessage = `Error: ${error.error.message}`;
+      errorMessage = `${error.error.message}`;
     } else {
-      // Erro no servidor
       if (error.status === 401) {
-        // Erro de autenticação
-        localStorage.removeItem('userToken'); // Remove o token JWT do localStorage
-        errorMessage = 'Nome de usuário ou senha inválidos.';
+        localStorage.removeItem(TOKEN_USER);
+        errorMessage = USER_MESSAGES.LOGIN_FAILED;
       } else if (error.status === 403) {
-        // Erro de autorização
-        localStorage.removeItem('userToken'); // Remove o token JWT do localStorage
-        errorMessage = 'Acesso negado!';
+        localStorage.removeItem(TOKEN_USER);
+        errorMessage = USER_MESSAGES.ACESSO_NEGADO;
       } else {
-        errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        errorMessage = `${error.message}`;
       }
     }
 
-    return throwError(errorMessage); // Retorna um observable com o erro
+    return throwError(errorMessage);
   }
 }
---
+++ vetsystemtech-frontend/src/app/management/shared/messages/user-messages.ts
@@ -0,0 +1,5 @@
+export const USER_MESSAGES = {
+  LOGIN_FAILED: 'Erro ao fazer login. Por favor, verifique suas credenciais e tente novamente.',
+  SERVER_ERROR: 'O servidor encontrou um erro. Por favor, tente novamente mais tarde.',
+  ACESSO_NEGADO: 'Acesso negado.',
+};
