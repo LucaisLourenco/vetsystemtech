fe495155869c683a2ca20679fb310f46ffb808f0
lucas.lourenco <lucaspeixe2018@gmail.com>
2024-03-23 11:23:12 -0300

    Lista de tutores

--- vetsystemtech-frontend/src/app/shared/shared.module.ts
+++ vetsystemtech-frontend/src/app/shared/shared.module.ts
@@ -3,15 +3,13 @@ import { CommonModule } from '@angular/common';
 import { AppMaterialModule } from './app-material/app-material.module';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
-import { GenericListComponent } from './components/generic-list/generic-list.component';
 
 
 
 @NgModule({
   declarations: [
     ErrorDialogComponent,
-    ConfirmationDialogComponent,
-    GenericListComponent
+    ConfirmationDialogComponent
   ],
   imports: [
     CommonModule,
