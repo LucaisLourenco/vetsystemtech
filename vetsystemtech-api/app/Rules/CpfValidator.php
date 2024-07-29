380d03b937261bcfe14ac25247bacada7a1ce4ea
lucas.lourenco <lucaspeixe2018@gmail.com>
2024-07-29 18:09:23 -0300

    Melhoria: Ajuste nas mensagens de erros e no controlador de tutores.

--- vetsystemtech-api/app/Rules/CpfValidator.php
+++ vetsystemtech-api/app/Rules/CpfValidator.php
@@ -2,6 +2,7 @@
 
 namespace App\Rules;
 
+use App\Helpers\DocumentsValidate;
 use App\Messages\MessageSystem;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
@@ -19,56 +20,8 @@ class CpfValidator implements ValidationRule
     {
         $cpf = preg_replace('/\D/', '', $value);
 
-        if (!$this->isValidCpf($cpf)) {
+        if (!DocumentsValidate::isValidCpf($cpf)) {
             $fail(MessageSystem::SYS002);
         }
-    }
-
-    /**
-     * @param string $cpf
-     * @return bool
-     */
-    private function isValidCpf(string $cpf): bool
-    {
-        if (strlen($cpf) !== 11 || preg_match('/(\d)\1{10}/', $cpf)) {
-            return false;
-        }
-
-        return $this->calculateChecksum($cpf) === substr($cpf, 9);
-    }
-
-    /**
-     * @param string $cpf
-     * @return string
-     */
-    private function calculateChecksum(string $cpf): string
-    {
-        $firstDigit = $this->calculateDigit($cpf, 10);
-        $secondDigit = $this->calculateDigit($cpf, 11, $firstDigit);
-        return $firstDigit . $secondDigit;
-    }
-
-    /**
-     * @param string $cpf
-     * @param int $weight
-     * @param int|null $firstDigit
-     * @return int
-     */
-    private function calculateDigit(string $cpf, int $weight, int $firstDigit = null): int
-    {
-        $sum = 0;
-        $length = $firstDigit === null ? 9 : 10;
-
-        for ($i = 0; $i < $length; $i++) {
-            $sum += $weight * (int)$cpf[$i];
-            $weight--;
-        }
-
-        if ($firstDigit !== null) {
-            $sum += 2 * $firstDigit;
-        }
-
-        $remainder = $sum % 11;
-        return $remainder < 2 ? 0 : 11 - $remainder;
     }
 }
