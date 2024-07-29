5a68de1e947dd91d43b8f5cf759de54c8cd872ea
lucas.lourenco <lucaspeixe2018@gmail.com>
2024-07-29 12:42:07 -0300

    .

--- vetsystemtech-frontend/src/app/management/tutors/tutors.component.ts
+++ vetsystemtech-frontend/src/app/management/tutors/tutors.component.ts
@@ -3,15 +3,12 @@ import {TutorsService} from "./services/tutors.service";
 import {MatDialog} from "@angular/material/dialog";
 import {ActivatedRoute, Router} from "@angular/router";
 import {MatSnackBar} from "@angular/material/snack-bar";
-import {catchError} from "rxjs/operators";
-import {Observable, of} from "rxjs";
 import {ErrorDialogComponent} from "../../shared/components/error-dialog/error-dialog.component";
 import {Tutor} from "./model/tutor";
 import {ConfirmationDialogComponent} from "../../shared/components/confirmation-dialog/confirmation-dialog.component";
 import {TutorLabelsEnum} from "../shared/enums/tutor-labels.enum";
 import {TutorMessages} from "../shared/messages/tutor-messages";
 import {CREATE, EDIT} from "./utils/tutors-routes";
-import {PaginationService} from "ngx-pagination";
 import {PaginationBaseComponent} from "../../core/base/pagination-base/pagination-base.component";
 
 @Component({
@@ -52,7 +49,7 @@ export class TutorsComponent extends PaginationBaseComponent<any> {
 
     dialogRef.afterClosed().subscribe((result: boolean) => {
       if (result) {
-        this.tutorsService.delete(tutor.cpf).subscribe(
+        this.tutorsService.delete(tutor.id).subscribe(
           () => {
             this.loadItems(1);
             this.snackBar.open(TutorMessages.TUR0001, 'x', {
